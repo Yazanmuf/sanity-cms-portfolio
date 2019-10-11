fd13f590cdb8a397202d4ca89632ef957f603ea2
Yazan Mufleh <ymufleh@gmail.com>
2019-10-11 08:56:45 -0400

    first commit of running sanity project, added projects and blog json objects. connected to front end gatsby portfolio project

---
+++ schemas/blog.js
@@ -0,0 +1,55 @@
+
+export default {
+
+    title: "Blog Post",
+    name: "blog-post",
+    type: "document",
+
+
+    fields: [
+
+        {
+            title: "Name",
+            name: "name",
+            type: "string",
+        },
+        {
+            title: "Slug",
+            name: "slug",
+            type: "slug",
+            options: {
+                source: "name",
+                slugify: input => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200)
+            }
+        }
+        ,
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'content',
+            title: 'Content',
+            type: 'array',
+            of: [
+                {
+                    type: 'block',
+                    marks: {
+                        decorators: [
+                            { title: 'Strong', value: 'strong' },
+                            { title: 'Emphasis', value: 'em' },
+                            { title: 'Code', value: 'code' }
+                        ],
+
+                    }
+                },
+
+            ]
+        }
+
+    ]
+}
---
+++ schemas/project.js
@@ -0,0 +1,66 @@
+
+export default {
+
+    title: "Project",
+    name: "project",
+    type: "document",
+
+
+    fields: [
+
+        {
+            title: "Name",
+            name: "name",
+            type: "string",
+        },
+        {
+            title: "Slug",
+            name: "slug",
+            type: "slug",
+            options: {
+                source: "name",
+                slugify: input => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200)
+            }
+        }
+        ,
+        {
+            title: "Link",
+            name: "link",
+            type: "url",
+        },
+        {
+            name: 'project_image',
+            title: 'Project Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            title: "Technologies",
+            name: "technologies",
+            type: "array",
+            of: [{ type: "string" }]
+        },
+        {
+            name: 'content',
+            title: 'Content',
+            type: 'array',
+            of: [
+                {
+                    type: 'block',
+                    marks: {
+                        decorators: [
+                            { title: 'Strong', value: 'strong' },
+                            { title: 'Emphasis', value: 'em' },
+                            { title: 'Code', value: 'code' }
+                        ],
+
+                    }
+                },
+
+            ]
+        }
+
+    ]
+}
