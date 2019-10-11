2cd66f7d1cbb081224b62d9d8d68321015c89e43
Yazan Mufleh <ymufleh@gmail.com>
2019-10-11 14:25:41 -0400

    added image as a higher level type in schema to avoid graphql deploy error

--- schemas/blog.js
+++ schemas/blog.js
@@ -48,23 +48,16 @@ export default {
                     }
                 },
                 {
+                    name: 'image',
+                    title: 'Image',
                     type: 'image',
-                    fields: [
-                        {
-                            type: 'text',
-                            name: 'alt',
-                            title: 'Alternative text',
-                            description: `Some of your visitors cannot see images, 
-            be they blind, color-blind, low-sighted; 
-            alternative text is of great help for those 
-            people that can rely on it to have a good idea of 
-            what\'s on your page.`,
-                            options: {
-                                isHighlighted: true
-                            }
-                        }
-                    ]
+                    options: {
+                        hotspot: true,
+                    }
                 }
+
+
+
 
             ]
         }
